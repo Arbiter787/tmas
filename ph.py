7ed966efe5571a35a6c72e0a2d358bb5959abbf9
wmss <applesinov@gmail.com>
2024-05-06 15:23:33 -0700

    moved duplicate code into separate file

--- ph.py
+++ ph.py
@@ -1,82 +1,10 @@
 #!/usr/bin/python
 
-# Made with heavy reference from the Atlas Scientific sample UART sensor code.
-
-import serial
-import sys
-import time
-import string
-from serial import SerialException
-
-# sends string command to sensor on serial. returns True if command was successfully sent.
-def send_command(serial: serial.Serial, command: string):
-    buf = command + "\r"    # add carriage return
-    try:
-        serial.write(buf.encode('utf-8'))
-        return True
-    except SerialException as e:
-        return "Error, " + e
-
-
-# reads a line from serial.
-def read_line(serial: serial.Serial):
-    lsl = len(b'\r')
-    line_buffer = []
-    while True:
-        next_char = serial.read(1)
-        if next_char == b'':
-            break
-        line_buffer.append(next_char)
-        if len(line_buffer) >= lsl and line_buffer[-lsl:] == [b'\r']:
-            break
-    return b''.join(line_buffer)
-
-
-# reads multiple lines from serial.
-def read_lines(serial: serial.Serial):
-    lines = []
-    try:
-        while True:
-            line = read_line(serial)
-            if not line:
-                break
-                serial.flush_input()
-            lines.append(line)
-        return lines
-    except SerialException as e:
-        return "Error, " + e
-
-# reads ph information from sensor on serial_port
-def read_ph(serial_port):
-    try:
-        ser = serial.Serial(serial_port, 9600, timeout=0)
-    except SerialException as e:
-        return "Error, " + e
-    
-    cmd_result = send_command(ser, "R")
-    if cmd_result != True:
-        return cmd_result
-
-    time.sleep(1.3)
-    lines = read_lines(ser)
-    if type(lines) == string:
-        return lines
-    for i in range(len(lines)):
-        lines[i] = lines[i].decode('utf-8')
-        if lines[i][-1] == "\r":
-            lines[i] = lines[i][:-1]      # strip carriage return
-    
-    ph = lines[0]
-    if lines[1] != "*OK":
-        return "Error, sensor error with status " + lines[1] + " Reported pH " + ph
-    else:
-        return ph
-    
-
+import digital
 
 if __name__ == "__main__":
     port = "/dev/serial0"
-    ph_info = read_ph(port)
+    ph_info = digital.read_data(port)
 
     print("PH", ph_info)
 
