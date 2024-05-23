1491768261100648008bb26a1dac273d84d9a06e
Luke <applesinov@gmail.com>
2024-05-23 15:50:19 -0700

    begin analog dev

---
+++ ph_analog.py
@@ -0,0 +1,39 @@
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+import sys
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+adc_max = 43520
+adc_min = 32700
+
+# Create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# Create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# Create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# Create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P1)
+
+raw_adc = (chan0.value)
+
+if (raw_adc == 0) or (raw_adc < (0.75 * adc_min)) or (raw_adc > 60000):
+	print('Water_Level -999')     # Error code 999 = hardware error with sensor or ADC
+	quit()
+else:
+	if (raw_adc > adc_max):
+		raw_adc = adc_max
+
+	percentage = 1 - ((raw_adc - adc_min) * 1) / (adc_max - adc_min)
+
+	if (percentage < 0):
+		percentage = 0
+
+	print('PH', (1 + round(percentage * 4, 1)))
+	print('RAW ADC', raw_adc)
